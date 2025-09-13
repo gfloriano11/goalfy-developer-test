41cc67da054b8096b96f35c7b1753d2f8c24f146
gfloriano11 <gflorianodev@gmail.com>
2025-09-13 13:53:57 -0300

    feat: create routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Clients from './pages/Clients.tsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {index: true, element: <App/>},
+      {path: 'clients', element: <Clients/>}
+    ]
+  }
+]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
