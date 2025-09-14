d6244ab463e06f6ee85cddf39a9fe385b59e411d
gfloriano11 <gflorianodev@gmail.com>
2025-09-14 10:42:26 -0300

    style: table head

--- src/components/registers/Table.tsx
+++ src/components/registers/Table.tsx
@@ -1,27 +1,46 @@
-import { TextCursor } from "lucide-react";
+import { AtSign, List, Phone, TextCursor } from "lucide-react";
 import styled from "styled-components";
 
 const ClientsTable = styled.table`
+    width: 80vw;
     font-family: "Red Hat Text";
-    /* font-weight: 400; */
     color: #666D73;
     font-size: 14px;
-    border-collapse: collapse;
-
-    & tr{
-        background-color: red;
+    /* border-collapse: collapse; */
+    border: 1px solid #E8E8E8;
+    
+    & thead{
+        width: 100%;
+        border-bottom: 1px solid #E8E8E8;
     }
 
-    & th{
-        font-weight: 500;
+    & tr{
+        display: flex;
+        align-items: center;
+        background-color: white;
     }
 `
 
-const Name = styled.th`
+const Th = styled.th`
+    min-width: 140px;
+    position: relative;
+    font-weight: 500;
+    padding: 8px 80px 8px 16px;
     display: flex;
-    justify-content: left;
-    padding-left: 20px;
-    width: 200px;
+    align-items: center;
+    gap: 6px;
+    color: #666D73;
+    font-size: 14px;
+
+    &:not(:last-child)::after {
+        content: '';
+        position: absolute;
+        top: 20%;
+        bottom: 20%;
+        right: 0;
+        width: 1px;
+        background-color: #e0e0e0;
+    }
 `;
 function Table(){
 
@@ -29,11 +48,12 @@ function Table(){
         <ClientsTable>
             <thead>
                 <tr>
-                    <Name> <TextCursor size={"16px"}/> Nome do Cliente</Name>
-                    <th>E-mail</th>
-                    <th>CNPJ</th>
-                    <th>Endereço</th>
-                    <th>Cidade</th>
+                    <Th> <TextCursor size={"16px"} color="#949FA6"/> Nome do Cliente</Th>
+                    <Th> <AtSign size={"16px"} color="#949FA6"/> Email</Th>
+                    <Th> <Phone size={"16px"} color="#949FA6"/> Telefone</Th>
+                    <Th> <List size={"16px"} color="#949FA6"/> CNPJ</Th>
+                    <Th> <TextCursor size={"16px"} color="#949FA6"/> Endereço</Th>
+                    <Th> <TextCursor size={"16px"} color="#949FA6"/> Cidade</Th>
                 </tr>
             </thead>
             <tbody>
