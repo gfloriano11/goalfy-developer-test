8b107afa87e687dee1aacd77a686c6e5bf09a293
gfloriano11 <gflorianodev@gmail.com>
2025-09-14 09:59:32 -0300

    feat: componentize options container

---
+++ src/components/optionsContainer/optionsContainer.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import PurpleButton from "../PurpleButton";
+import { PlusCircle, SearchIcon } from "lucide-react";
+
+function OptionsContainer(){
+    const OptionsContainer = styled.div`
+        display: flex;
+        align-items: center;
+        width: 80vw;
+        height: 32px;
+        gap: 20px;
+    `
+
+        const SearchContainer = styled.div`
+        height: 100%;
+        max-height: 32px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #F2F2F2;
+        border: 1px solid #E8E8E8;
+        border-radius: 8px;
+    `
+
+    const Search = styled.input`
+        height: 100%;
+        padding: 0;
+        border-radius: 0 8px 8px 0;
+        background-color: #F2F2F2;
+        border: none;
+        color: #949FA6;
+        font-family: "Red Hat Text";
+        outline: none;
+    `
+
+    const StyledSearch = styled(SearchIcon)`
+        color: #949fa6;
+        padding-left: 4px;
+        padding-right: 4px;
+    `;
+
+    const RegistersInfo = styled.p`
+        font-family: "Red Hat Text";
+        color: #949FA6;
+        font-size: 16px;
+        font-weight: 500;
+    `;
+    
+    return(
+        <OptionsContainer>
+            <PurpleButton icon={PlusCircle} text="Novo Registro"/>
+            <SearchContainer>
+                <StyledSearch size="24px"/>
+                <Search placeholder="Pesquisar..."/>
+            </SearchContainer>
+            <RegistersInfo>86 registros</RegistersInfo>
+        </OptionsContainer>
+    )
+}
+
+export default OptionsContainer;
--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,7 +1,6 @@
-import { PlusCircle, SearchIcon } from "lucide-react";
 import Navbar from "../components/navbar/Navbar";
-import PurpleButton from "../components/PurpleButton";
 import styled from "styled-components";
+import OptionsContainer from "../components/optionsContainer/optionsContainer";
 
 
 function Clients(){
@@ -12,60 +11,10 @@ function Clients(){
         align-items: center;
         gap: 30px;
     `
-
-    const OptionsContainer = styled.div`
-        display: flex;
-        align-items: center;
-        width: 80vw;
-        height: 32px;
-        gap: 20px;
-    `
-
-    const SearchContainer = styled.div`
-        height: 100%;
-        max-height: 32px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #F2F2F2;
-        border: 1px solid #E8E8E8;
-        border-radius: 8px;
-    `
-
-    const Search = styled.input`
-        height: 100%;
-        padding: 0;
-        border-radius: 0 8px 8px 0;
-        background-color: #F2F2F2;
-        border: none;
-        color: #949FA6;
-        font-family: "Red Hat Text";
-        outline: none;
-    `
-
-    const StyledSearch = styled(SearchIcon)`
-        color: #949fa6;
-        padding-left: 4px;
-        padding-right: 4px;
-    `;
-
-    const RegistersInfo = styled.p`
-        font-family: "Red Hat Text";
-        color: #949FA6;
-        font-size: 16px;
-        font-weight: 500;
-    `;
     return(
     <MainContainer>
         <Navbar/>
-        <OptionsContainer>
-            <PurpleButton icon={PlusCircle} text="Novo Registro"/>
-            <SearchContainer>
-                <StyledSearch size="24px"/>
-                <Search placeholder="Pesquisar..."/>
-            </SearchContainer>
-            <RegistersInfo>86 registros</RegistersInfo>
-        </OptionsContainer>
+        <OptionsContainer/>
     </MainContainer>
     )
 }
