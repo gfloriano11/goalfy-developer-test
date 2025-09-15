41a52309ff5cdb57af9fddef7a4ab5377b046fdb
gfloriano11 <gflorianodev@gmail.com>
2025-09-14 22:56:41 -0300

    feat: function to add client by CEP

--- app/model/clients.js
+++ app/model/clients.js
@@ -21,4 +21,58 @@ async function getClients(req, res){
     connect.endConnection(connection);
 }
 
-export default getClients;
+async function createClient(req, res){
+    const connection = connect.getConnection();
+
+    const CEP = req.body.CEP;
+
+    const addressInfo = await fetch(`https://viacep.com.br/ws/${CEP}/json`, {
+        method: 'GET',
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+    })
+
+    const data = await addressInfo.json();
+
+    const city = data.localidade;
+    const state = data.estado;
+    const UF = data.uf;
+    const address = data.logradouro;
+    const neighborhood = data.bairro;
+
+    const values = [
+        req.body.fullname,
+        req.body.email,
+        req.body.phone,
+        req.body.CNPJ,
+        address,
+        city,
+        state,
+        UF,
+        CEP,
+        neighborhood
+    ];
+
+    const query = `
+        INSERT INTO clients
+        (fullname, email, phone, CNPJ, address, city, state, UF, CEP, neighborhood)
+        VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    `
+
+    connection.query(query, values, (error, data) => {
+        if(error){
+            res.status(500).json(error);
+        }
+
+        res.status(200).json(data);
+    })
+
+    connect.endConnection(connection);
+}
+
+export default {
+    getClients,
+    createClient
+};
